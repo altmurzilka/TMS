75fb0702e388f9a947567e5e8dd41cc7e52688d5
altyn <altmurzilka@gmail.com>
2020-09-17 00:27:48 +0600

    seems done still need to refactor

--- ProjectArrows/ProjectArrows/ViewController.swift
+++ ProjectArrows/ProjectArrows/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     let myView = UIView()
-    var num: Int = 1
+    
+    var step: CGFloat = 50
     
     let buttonUp = UIButton()
     let buttonDown = UIButton()
     let buttonRight = UIButton()
     let buttonLeft = UIButton()
-    
-    var toUp = false
     
     let configuration = UIImage.SymbolConfiguration(pointSize: 100)
     
@@ -34,57 +33,58 @@ class ViewController: UIViewController {
         self.view.addSubview(myView)
         
         buttonUp.setImage(UIImage(systemName: "arrow.up.square", withConfiguration: configuration), for: .normal)
-        buttonUp.frame = CGRect(x: self.view.frame.size.width / 2 - 25, y: self.view.frame.size.height / 2 + 200, width: 50, height: 50)
+        buttonUp.frame = CGRect(x: width / 2 - 25, y: height / 2 + 200, width: 50, height: 50)
         buttonUp.addTarget(self, action: #selector(buttonActionUp), for: .touchUpInside)
-        num = 1
         self.view.addSubview(buttonUp)
         
         buttonDown.setImage(UIImage(systemName: "arrow.down.square", withConfiguration: configuration), for: .normal)
-        buttonDown.frame = CGRect(x: self.view.frame.size.width / 2 - 25, y: self.view.frame.size.height / 2 + 250, width: 50, height: 50)
+        buttonDown.frame = CGRect(x: width / 2 - 25, y: height / 2 + 250, width: 50, height: 50)
         buttonDown.addTarget(self, action: #selector(buttonActionDown), for: .touchUpInside)
-        num = 2
         self.view.addSubview(buttonDown)
         
         buttonLeft.setImage(UIImage(systemName: "arrow.left.square", withConfiguration: configuration), for: .normal)
-        buttonLeft.frame = CGRect(x: self.view.frame.size.width / 2 - 75, y: self.view.frame.size.height / 2 + 250, width: 50, height: 50)
+        buttonLeft.frame = CGRect(x: width / 2 - 75, y: height / 2 + 250, width: 50, height: 50)
         buttonLeft.addTarget(self, action: #selector(buttonActionLeft), for: .touchUpInside)
-        num = 3
         self.view.addSubview(buttonLeft)
         
         buttonRight.setImage(UIImage(systemName: "arrow.right.square", withConfiguration: configuration), for: .normal)
-        buttonRight.frame = CGRect(x: self.view.frame.size.width / 2 + 25, y: self.view.frame.size.height / 2 + 250, width: 50, height: 50)
+        buttonRight.frame = CGRect(x: width / 2 + 25, y: height / 2 + 250, width: 50, height: 50)
         buttonRight.addTarget(self, action: #selector(buttonActionRight), for: .touchUpInside)
-        num = 4
         self.view.addSubview(buttonRight)
         
     }
     
     @objc func buttonActionUp(sender: UIButton!) {
-        toUp = true
-        
-        let width = myView.frame.size.width / 2 - 50
-        var height = myView.frame.size.height / 2 - 200
-        
-        if self.toUp == true && height != self.view.frame.size.width - 50 {
-            height -= 2
-            myView.frame = CGRect(x: width, y: height, width: 100, height: 100)
+        if myView.frame.origin.y <= 10.0 {
+            //   print("error")
+        } else {
+            myView.frame.origin.y -= step
+            //  print("frame - \(myView.frame.origin.y)")
+            //  print(myView.bounds.origin.y)
         }
-        self.toUp = !self.toUp
     }
     
     @objc func buttonActionDown(sender: UIButton!) {
-        
+        let height = self.view.frame.size.height
+        if myView.frame.origin.y >= height / 2 + 70 {
+        } else {
+            myView.frame.origin.y += step
+        }
     }
     
     @objc func buttonActionLeft(sender: UIButton!) {
-        
-        
+        if myView.frame.origin.x <= 10.0 {
+        } else {
+            myView.frame.origin.x -= step
+        }
     }
     
     @objc func buttonActionRight(sender: UIButton!) {
         let width = self.view.frame.size.width
-        let height = self.view.frame.size.height
-        
+        if myView.frame.origin.x >= width - 110.0 {
+        } else {
+            myView.frame.origin.x += step
+        }
     }
     
     func newColor() -> UIColor {
