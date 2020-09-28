5b31f23c77afc6625a9ab1bbf9dcd6f073ed9df2
altyn <altmurzilka@gmail.com>
2020-09-29 00:02:38 +0600

    works, but strange

--- ProjectOnTap/ProjectOnTap/ViewController.swift
+++ ProjectOnTap/ProjectOnTap/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     
     var viewArray = [UIView]()
     var tagArray = [Int]()
+    var tagsToDeleteArray = [Int]()
     
     var counter = 1
     
@@ -32,53 +33,79 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(tap)
     }
     
+    
+    // taking the position of user's press
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let position = touch.location(in: view)
             x = position.x
             y = position.y
+            print("\(x) \(y)")
+            isIntersects()
         }
     }
+    
     
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
         guard sender.view != nil else { return }
         
         if sender.state == .ended {
             
+            
+            for i in viewArray {
+                if tagsToDeleteArray.count > 0 {
+                    removeSubview(tag: i.tag)
+                }
+                tagsToDeleteArray.removeAll()
+            }
+            
+            
             let myView = UIView()
-            x = x - 50
-            y = y - 50
-            myView.frame = CGRect(x: self.x, y: self.y, width: 100, height: 100)
+            myView.frame = CGRect(x: self.x-50, y: self.y-50, width: 100, height: 100)
             myView.layer.cornerRadius = 0.5 * myView.bounds.size.width
             myView.tag = self.counter
             myView.backgroundColor = self.randomColor()
+            viewArray.append(myView)
             self.view.addSubview(myView)
             
-            viewArray.append(myView)
-            
-            for i in viewArray {
-                if i.frame.intersects(myView.frame) {
-                    tagArray.append(myView.tag)
-                    removeSubview()
+        }
+        
+        print(viewArray)
+        tagArray.append(counter)
+        counter = counter + 1
+        
+        print(tagArray)
+        print(tagsToDeleteArray)
+    }
+    
+    
+    func isIntersects() {
+        for i in viewArray {
+            if CGFloat(x-101)...CGFloat(x+101) ~=  i.frame.origin.x {
+                if CGFloat(y-101)...CGFloat(y+101) ~=  i.frame.origin.y {
+                    tagsToDeleteArray.append(i.tag)
+                }
+            } else if CGFloat(y-101)...CGFloat(y+101) ~=  i.frame.origin.y {
+                if CGFloat(x-101)...CGFloat(x+101) ~=  i.frame.origin.x {
+                    tagsToDeleteArray.append(i.tag)
                 }
             }
         }
-        counter = counter + 1
     }
     
-    func removeSubview(){
-        
-        print(viewArray)
-        
-        for i in tagArray {
+    
+    func removeSubview(tag: Int) {
+        for i in tagsToDeleteArray {
             if let viewWithTag = self.view.viewWithTag(i) {
                 viewWithTag.removeFromSuperview()
             }
         }
-        
     }
+
     
 }
+
+
 
 
 
