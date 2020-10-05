74cfa524bf1dee203e18fc647e3b9a08978dc324
altyn <altmurzilka@gmail.com>
2020-10-06 03:26:32 +0600

    road bug fixed

--- ProjectRace/ProjectRace/NewGameViewController.swift
+++ ProjectRace/ProjectRace/NewGameViewController.swift
@@ -16,14 +16,17 @@ class NewGameViewController: UIViewController {
     @IBOutlet var leftButton: UIButton!
     @IBOutlet var rightButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    let contentImages = ["boy", "girl", "ball", "log"]
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         roadMove()
+        print(view.frame.size.width)
+        
+        var imageView : UIImageView
+        imageView  = UIImageView(frame:CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>));
+        imageView.image = UIImage(named:"image.jpg")
+        self.view.addSubview(imageView)
     }
     
     func roadMove() {
@@ -34,11 +37,19 @@ class NewGameViewController: UIViewController {
     }
     
     @IBAction func tapLeft(_ sender: UIButton) {
-        self.carImage.bounds.origin.x -= 30
-        leftButton.showsTouchWhenHighlighted = true
+        if self.carImage.frame.origin.x <= 25 {
+            self.navigationController?.popToRootViewController(animated: true)
+        } else {
+            self.carImage.frame.origin.x -= 30
+            leftButton.showsTouchWhenHighlighted = true
+        }
     }
     @IBAction func tapRight(_ sender: UIButton) {
-        self.carImage.bounds.origin.x += 30
-        rightButton.showsTouchWhenHighlighted = true
+        if self.carImage.frame.maxX >= view.frame.size.width - 25 {
+            self.navigationController?.popToRootViewController(animated: true)
+        } else {
+            self.carImage.frame.origin.x += 30
+            rightButton.showsTouchWhenHighlighted = true
+        }
     }
 }
