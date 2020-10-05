bd4fadf90ea76638b78f264b1cfe6218c5266fda
altyn <altmurzilka@gmail.com>
2020-10-06 02:08:59 +0600

    don't work properly

--- ProjectRace/ProjectRace/NewGameViewController.swift
+++ ProjectRace/ProjectRace/NewGameViewController.swift
@@ -8,18 +8,37 @@
 import UIKit
 
 class NewGameViewController: UIViewController {
-
-    @IBOutlet var roadView: UIView!
+    
+    @IBOutlet var supportingRoadView: UIImageView!
+    @IBOutlet var roadView: UIImageView!
+    @IBOutlet var carImage: UIImageView!
+    
+    @IBOutlet var leftButton: UIButton!
+    @IBOutlet var rightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UIView.animate(withDuration: 4.0, delay: 0, options: [.repeat, .autoreverse, .curveLinear], animations: {
-            self.roadView.frame.origin.y = self.view.bounds.height + self.roadView.frame.height - 50
-
-        }, completion: nil)
-        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        roadMove()
+    }
     
-
+    func roadMove() {
+        UIView.animate(withDuration: 2, delay: 0.0, options: [.repeat, .curveLinear], animations: {
+            self.roadView.frame = self.roadView.frame.offsetBy(dx: 0.0, dy: self.roadView.frame.size.height)
+            self.supportingRoadView.frame = self.supportingRoadView.frame.offsetBy(dx: 0.0, dy: self.supportingRoadView.frame.size.height)
+        }, completion: nil)
+    }
+    
+    @IBAction func tapLeft(_ sender: UIButton) {
+        self.carImage.bounds.origin.x -= 30
+        leftButton.showsTouchWhenHighlighted = true
+    }
+    @IBAction func tapRight(_ sender: UIButton) {
+        self.carImage.bounds.origin.x += 30
+        rightButton.showsTouchWhenHighlighted = true
+    }
 }
