45cb2bb558185dbd4ccff34cb94c722cb49fc68f
altyn <altmurzilka@gmail.com>
2020-10-12 21:04:44 +0600

    buttons styling added

--- ProjectRace/ProjectRace/Extensions.swift
+++ ProjectRace/ProjectRace/Extensions.swift
@@ -10,12 +10,21 @@ import UIKit
 extension UIButton {
     func dropShadow() {
         self.layer.masksToBounds = false
-        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowColor = UIColor.red.cgColor
         self.layer.shadowOpacity = 1
         self.layer.shadowOffset = CGSize.zero
         self.layer.shadowRadius = 10
         self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
         self.layer.shouldRasterize = true
         self.layer.rasterizationScale = UIScreen.main.scale
+    }
+    
+    func customColor() {
+        self.clipsToBounds = true
+        self.tintColor = .red
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 10
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.borderWidth = 1.0
     }
 }
--- ProjectRace/ProjectRace/ViewController.swift
+++ ProjectRace/ProjectRace/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var newGameButton: UIButton!
+    @IBOutlet var recordsButton: UIButton!
+    @IBOutlet var settingsButton: UIButton!
+    
+    var buttons = [UIButton]()
+    
     @IBAction func NewGameButton(_ sender: UIButton) {
         let controller = self.storyboard?.instantiateViewController(withIdentifier: "NewGameViewController") as! NewGameViewController
         
@@ -25,6 +31,17 @@ class ViewController: UIViewController {
         let controller = self.storyboard?.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
         
         self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        buttons = [self.newGameButton, self.recordsButton, self.settingsButton]
+        
+        for i in buttons {
+            i.customColor()
+            i.dropShadow()
+        }
     }
     
 }
