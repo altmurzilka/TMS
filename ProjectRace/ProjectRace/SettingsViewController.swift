206f2dd0b241a8702bbfebcca8127b462610a9a9
altyn <altmurzilka@gmail.com>
2020-10-25 02:13:30 +0600

    segmented control

--- ProjectRace/ProjectRace/SettingsViewController.swift
+++ ProjectRace/ProjectRace/SettingsViewController.swift
@@ -18,20 +18,51 @@ class SettingsViewController: UIViewController {
     
     let obstacles = ["tree", "rock", "ball", "log"]
     let vehicleColors = ["red", "green", "yellow", "blue"]
-
+    var pickedColor: Colors?
+    
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var vehicleColorSegmentedControl: UISegmentedControl!
     @IBOutlet var ObstacleTypePicker: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        ObstacleTypePicker.delegate = self
+        ObstacleTypePicker.dataSource = self
     }
     
-
+    @IBAction func colorChange(_ sender: UISegmentedControl) {
+        switch vehicleColorSegmentedControl.selectedSegmentIndex {
+        case 0:
+            pickedColor = .blue
+        case 1:
+            pickedColor = .green
+        case 2:
+            pickedColor = .yellow
+        default:
+            pickedColor = .red
+        }
+    }
+    
     @IBAction func cancelButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
     
 }
+
+
+extension SettingsViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return obstacles.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return obstacles[row]
+    }
+}
+
