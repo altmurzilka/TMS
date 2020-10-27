19533d309464daa8f130bb2f9c77d729643eaafb
altyn <altmurzilka@gmail.com>
2020-10-27 22:08:03 +0600

    projectXib

---
+++ ProjectXIB/ProjectXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectXIB
+//
+//  Created by Алтын on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
