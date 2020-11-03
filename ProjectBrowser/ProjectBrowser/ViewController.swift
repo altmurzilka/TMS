498f8aa2932e3ab2f605b6193bad00b5fff55db9
altyn <altmurzilka@gmail.com>
2020-11-03 22:25:15 +0600

    my browser sucks

---
+++ ProjectBrowser/ProjectBrowser/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProjectBrowser
+//
+//  Created by Алтын on 11/3/20.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+    
+    @IBAction func buttonTapped() {
+        let vc = SFSafariViewController(url: URL(string: "")!)
+        present(vc, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+    }
+}
+
